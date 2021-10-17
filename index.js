aabbd26cb3c27ad3c5088ee8b1bd7ab1d32b497f
Ernesto Resende <dev@ernestoresende.com>
2021-10-17 14:51:38 -0300

    feat: exposed separate endpoints for chat & video tokens

--- index.js
+++ index.js
@@ -4,16 +4,22 @@ const http = require("http");
 const path = require("path");
 const express = require("express");
 const cors = require("cors");
-const tokenGenerator = require("./token_generator");
+const videoTokenGenerator = require("./video_token_generator");
+const chatTokenGenerator = require("./chat_token_generator");
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")), cors());
 
-app.get("/", function (request, response) {
+app.get("/video-token", function (request, response) {
   const identity = request.query.identity || "identity";
   const room = request.query.room;
-  response.send(tokenGenerator(identity, room));
+  response.send(videoTokenGenerator(identity, room));
+});
+
+app.get("/chat-token", function (request, response) {
+  const identity = request.query.identity || "identity";
+  response.send(chatTokenGenerator(identity));
 });
 
 const server = http.createServer(app);
